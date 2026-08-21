c5add611f67830eee9a0880beb5735f11fe12a24
Muhammad Abdullah <abdullahwpdev083@gmail.com>
2026-08-21 17:21:17 +0500

    Add TOOLIVO splash activity

---
+++ toolivo/app/src/main/java/com/toolivo/smarttools/SplashActivity.kt
@@ -0,0 +1,7 @@
+package com.toolivo.smarttools
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+class SplashActivity:AppCompatActivity(){override fun onCreate(savedInstanceState:Bundle?){super.onCreate(savedInstanceState);Handler(Looper.getMainLooper()).postDelayed({startActivity(Intent(this,MainActivity::class.java));finish()},900)}}
